4057935c2f425301f65a1aa648e2b77b60feee33
a-santoso <asantoso@seas.upenn.edu>
2022-02-19 15:21:12 +0700

    Refined

--- ethereum_query.py
+++ ethereum_query.py
@@ -23,7 +23,7 @@ def get_gas_price(tx):
     return gas_price
 
 def get_gas(tx):
-    gas = 1 #YOUR CODE HERE
+    gas = w3.eth.get_transaction_receipt(tx).get('gasUsed')
     return gas
 
 def get_transaction_cost(tx):
